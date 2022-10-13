a9976d29a82b9fb4156e4cda8c36f18a1dfb8c64
ViktorOnyedim <viktoronyedim@gmail.com>
2022-10-13 16:22:48 -0700

    changed main data type to int

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,12 @@
 
 /**
  * main - Prints a string using the puts function
+ *
+ * Return: 0 if the program executed successfully
  */
 
-void main(void)
+int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
+	return (0);
 }
