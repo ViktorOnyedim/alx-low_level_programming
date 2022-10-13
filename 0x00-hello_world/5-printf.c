4f6668177d942ebc9770338edb0310881bf87410
ViktorOnyedim <viktoronyedim@gmail.com>
2022-10-13 16:43:14 -0700

    Added 5-printf.c program

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Print a string using printf()
+ *
+ * Return: 0 if program executed successfully
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
