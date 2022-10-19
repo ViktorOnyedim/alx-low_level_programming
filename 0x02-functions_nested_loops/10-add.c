0745436c817be389541f68031947dc11374464a5
ViktorOnyedim <viktoronyedim@gmail.com>
2022-10-19 04:39:15 +0000

    Added 10-add.c and updated main.h

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @x: First integer to add
+ * @y: Second integer to add
+ *
+ * Return: result
+ */
+int add(int x, int y)
+{
+	int result;
+
+	result = x + y;
+	return (result);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,3 +5,6 @@ int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int n);
+int print_last_digit(int n);
+void jack_bauer(void);
+int add(int x, int y);
