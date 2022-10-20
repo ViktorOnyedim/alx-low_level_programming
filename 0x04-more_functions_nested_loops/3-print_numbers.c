aa45278aa951ba6b45608c07ca0fac0cf34fd13b
ViktorOnyedim <viktoronyedim@gmail.com>
2022-10-20 19:37:43 +0000

    Added 3-print_numbers.c and updated main.h

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints numbers from 0 to 9
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
