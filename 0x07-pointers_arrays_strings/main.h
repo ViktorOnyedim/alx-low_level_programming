afc327dbd62ffcf21c5f164025942670b70a9ffd
ViktorOnyedim <viktoronyedim@gmail.com>
2022-11-01 11:31:26 +0000

    Added 0-memset.c and updated main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #define _MAIN_H_
 
 int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
 char *_strchr(char *s, char c);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
