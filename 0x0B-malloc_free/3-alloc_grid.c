b9e9508a3414214f72901c966bcae936e9ef7df9
ViktorOnyedim <viktoronyedim@gmail.com>
2022-11-10 16:16:07 +0000

    Modified 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,7 +17,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	gridout = (int *)malloc(height * sizeof(int *));
+	gridout = (int **)malloc(height * sizeof(int *));
 	if (gridout == NULL)
 	{
 		free(gridout);
