f2b84f753a44c00bcf7875dace07787faa7e55ca
ViktorOnyedim <viktoronyedim@gmail.com>
2022-11-12 01:07:17 +0000

    Added 2-calloc.c and updated main.h

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - Allocates memory for an array using malloc.
+ * @nmemb: number of members in the array
+ * @size: size of array in bytes
+ *
+ * Return: Pointer to allocated memory
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL || nmemb == 0 || size == 0)
+		return (NULL);
+	return (ptr);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -3,5 +3,6 @@
 
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
+void *_calloc(unsigned int nmemb, unsigned int size);
 
 #endif /* _MAIN_H */
