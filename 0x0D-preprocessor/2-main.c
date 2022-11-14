99041acd09a5462dd6b44ea4f955cf618aa115d6
ViktorOnyedim <viktoronyedim@gmail.com>
2022-11-14 15:25:21 +0000

    Added 2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the file it was compiled from
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
