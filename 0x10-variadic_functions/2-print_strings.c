d467b1130f6efdcd320d8aead950c4f712b5c25c
ViktorOnyedim <viktoronyedim@gmail.com>
2022-11-17 17:02:15 +0000

    Added 2-print_strings and updated variadic_functions.h

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,35 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_strings - Prints strings
+ * @seperator: string to be printed between the strings
+ * @n: number of strings passed to the function
+ *
+ * Return: Nothing.
+ */
+
+void print_strings(const char *seperator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list args;
+	char *string;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		string = va_arg(args, char *);
+		if (string == NULL)
+		{
+			string = "nil";
+		}
+		printf("%s", string);
+		if (seperator != NULL && i != n - 1)
+		{
+			printf("%s", seperator);
+		}
+	}
+	va_end(args);
+	printf("\n");
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,5 +4,6 @@
 int _putchar(char);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *seperator, const unsigned int n, ...);
+void print_strings(const char *seperator, const unsigned int n, ...);
 
 #endif
