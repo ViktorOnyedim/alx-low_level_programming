342ed329ad3e7908d1edc05e1c062eb4526e886a
ViktorOnyedim <viktoronyedim@gmail.com>
2022-11-25 17:34:12 +0000

    Modified 101-hello_holberton.asm file

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,20 +1,14 @@
-; Prints "Hello, Holberton" to the console using only system calls
+; Prints "Hello, Holberton" to the console using only printf
 ; Runs on 64-bit linux only.
 ; To assemble and run:
 ;
-;	nasm -felf64 101-hello_holberton.asm && ld 101-hello_holberton.0 && ./a.out
-
-	global	_start
-
-	section	.text
-_start	mov	rax, 1
-	mov	rdi, 1
-	mov	rsi, message
-	mov	rdx, 17
-	syscall
-	mov	rax, 60
-	xor	rdi, rdi
-	syscall
-
-	section	.data
-message:	db	"Hello, Holberton", 10
+;	
+global	main
+	extern	printf
+main:
+	mov	edi, format
+	xor	eax, eax
+	call	printf
+	mov	eax, 0
+	ret
+format: db	`Hello, Holberton\n`, 0
