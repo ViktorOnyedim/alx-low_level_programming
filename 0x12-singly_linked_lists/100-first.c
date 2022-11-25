ace75be38edbb3e8c552682a106cc5045fd82fb5
ViktorOnyedim <viktoronyedim@gmail.com>
2022-11-25 16:44:16 +0000

    Added 100-first.c and updated lists.h file

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * before_main - Prints string before the main function is executed
+ *
+ * Return: Nothing.
+ */
+
+void before_main()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
