124aff30bc3ebd57d7d1566bd47e00f477d7967f
ViktorOnyedim <viktoronyedim@gmail.com>
2022-12-13 15:50:39 +0000

    Modified 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,15 +10,9 @@
  * @filename: name of the file to create
  * @text_content: NULL terminated string to write to the file
  *
- * Return: 1 on success, -1 on failure (file cannot be created, 
+ * Return: 1 on success, -1 on failure (file cannot be created,
  * cannot be written, write "fails", etc...).
  */
-
-
-/**
- * creat() is equiv. to open() with flags = 0_CREAT|0_WRONLY|0_TRUNC
- */
-
 
 int create_file(const char *filename, char *text_content)
 {
@@ -30,17 +24,13 @@ int create_file(const char *filename, char *text_content)
 
 	for (count = 0; text_content[count] != '\0'; count++)
 		;
-	write(fd, text_content, count);
-	if (text_content == NULL)
-		(void)text_content;
-	/*
+
 	if (text_content != NULL)
 	{
-		count = 0;
-		while (text_content[count] != '\0')
-			count++;
+		for (count = 0; text_content[count] != '\0'; count++)
+			;
 		write(fd, text_content, count);
-	}*/
+	}
 	close(fd);
 
 	return (1);
