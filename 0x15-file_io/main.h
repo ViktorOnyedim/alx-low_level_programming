941f617f8ca39cc08082a6b79b9cf68702ffabbf
ViktorOnyedim <viktoronyedim@gmail.com>
2022-12-13 15:45:32 +0000

    Added 1-create_file and updated main.h

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stddef.h>
+#include <sys/types.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+
+#endif
