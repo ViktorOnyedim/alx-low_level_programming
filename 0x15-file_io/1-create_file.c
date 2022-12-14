523828ac3d0c01efa992bdfd376ea0e19404e866
ViktorOnyedim <viktoronyedim@gmail.com>
2022-12-14 09:18:27 +0000

    Modified 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,22 +16,27 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, count;
+	int fd;
+	size_t count;
+	ssize_t len = 0;
 
-	fd = creat(filename, 0600);
-	if (fd == -1 || filename == NULL)
+	if (filename == NULL)
 		return (-1);
 
+	fd = creat(filename, 0600);
+	if (fd == -1)
+		return (-1);
+
+	/* get text_content length */
 	for (count = 0; text_content[count] != '\0'; count++)
 		;
 
 	if (text_content != NULL)
-	{
-		for (count = 0; text_content[count] != '\0'; count++)
-			;
-		write(fd, text_content, count);
-	}
+		len = write(fd, text_content, count);
+
 	close(fd);
+	if (len == -1)
+		return (-1);
 
 	return (1);
 }
